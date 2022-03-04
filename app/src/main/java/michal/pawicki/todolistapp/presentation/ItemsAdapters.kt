2d8039e14cc740b2b9e9c5ec499e4496db95ca40
MichalPawicki <michal_pawicki@op.pl>
2022-03-04 22:34:59 +0100

    Uporządkowanie projektu part 2

--- app/src/main/java/michal/pawicki/todolistapp/presentation/ItemsAdapters.kt
+++ app/src/main/java/michal/pawicki/todolistapp/presentation/ItemsAdapters.kt
@@ -6,44 +6,55 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import michal.pawicki.todolistapp.databinding.ItemItemBinding
 
-class ItemsAdapters(private val action: (Int, Boolean)-> Unit,
-                    private val editItem: (Int) -> Unit,
-                    private val deleteItem: (Int) -> Unit) : RecyclerView.Adapter<ItemsAdapters.ViewHolder>() {
+class ItemsAdapters(
+    private val action: (Int, Boolean) -> Unit,
+    private val editItem: (Int) -> Unit,
+    private val deleteItem: (Int) -> Unit
+) : RecyclerView.Adapter<ItemsAdapters.ViewHolder>() {
     class ViewHolder(val binding: ItemItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     private val items = mutableListOf<ToDoItemUi>()
+
     @SuppressLint("NotifyDataSetChanged")
-    fun updateItems(items: List<ToDoItemUi>){
+    fun updateItems(items: List<ToDoItemUi>) {
         this.items.clear()
         this.items.addAll(items)
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ItemItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return ViewHolder(
+            ItemItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item  = items[position]
+        val item = items[position]
         holder.binding.categoryTxt.text = item.title
         holder.binding.contentTxt.text = item.note
         holder.binding.dataTxt.text = item.dateTxt
         holder.binding.itemsStatusButton.setOnCheckedChangeListener(null)
         holder.binding.itemsStatusButton.isChecked = item.status
         holder.binding.itemsStatusButton.setOnCheckedChangeListener { _, checked ->
-            if (item.status != checked){
+            if (item.status != checked) {
                 action(item.id, checked)
             }
         }
-        holder.itemView.setOnClickListener{
+        // -------------------Nasłuchiwanie na kliknięcie: włącza fragment_add_item ----------------
+        holder.itemView.setOnClickListener {
             editItem(item.id)
         }
+        //Nasłuchiwanie na przytrzymanie klikniecia: włącza okno z zapytaniem usunięcia powiadomienia
         holder.itemView.setOnLongClickListener {
             deleteItem(item.id)
             true
         }
     }
-
+    // -------------------Pobiera i zwraca wielkość listy ------------------------------------------
     override fun getItemCount(): Int {
         return items.size
     }
--- app/src/main/java/michal/pawicki/todolistapp/presentation/ItemsDestination.kt
+++ app/src/main/java/michal/pawicki/todolistapp/presentation/ItemsDestination.kt
@@ -1,6 +1,8 @@
 package michal.pawicki.todolistapp.presentation
 
+// -------------------Klasa zapieczętowana, gdzie rozszerzanie jest dopuszczalne -------------------
+
 sealed class ItemsDestination {   //Typ wyliczeniowy (Enum na "sterydach")
-    data class Detail(val id: Int): ItemsDestination()
+    data class Detail(val id: Int) : ItemsDestination()
     object NewItem : ItemsDestination()
 }
